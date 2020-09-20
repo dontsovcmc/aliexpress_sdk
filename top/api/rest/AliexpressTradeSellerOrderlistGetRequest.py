b54da38a1985bc884e495b23b7e784e5d9fe12c0
Evgeny Dontsov <don-and-home@mail.ru>
2020-09-21 00:03:44 +0300

    aliexpress SDK 2020-09-17

---
+++ top/api/rest/AliexpressTradeSellerOrderlistGetRequest.py
@@ -0,0 +1,11 @@
+'''
+Created by auto_sdk on 2020.08.10
+'''
+from top.api.base import RestApi
+class AliexpressTradeSellerOrderlistGetRequest(RestApi):
+	def __init__(self,domain='gw.api.taobao.com',port=80):
+		RestApi.__init__(self,domain, port)
+		self.param_aeop_order_query = None
+
+	def getapiname(self):
+		return 'aliexpress.trade.seller.orderlist.get'
